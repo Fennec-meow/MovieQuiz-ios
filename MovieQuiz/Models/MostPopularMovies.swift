f90755c0bb1216f68d674872c2183a793db2cabd
Fennec-meow <krisbojchenko@gail.com>
2024-11-26 18:49:12 +0300

    sprint_06

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by Kira on 20.11.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/NetworkClient.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkClient.swift
+//  MovieQuiz
+//
+//  Created by Kira on 19.11.2024.
+//
+
+import Foundation
